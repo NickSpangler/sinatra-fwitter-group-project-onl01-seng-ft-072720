4177ff56db917ebcc30a6a38ada66fae9bbc8e44
NickSpangler <Spangleyman@mac.com>
2020-09-11 09:50:36 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        erb :"users/signup"
+      end
+    
+      get '/signup_error' do
+        erb :"/users/signup_error"
+      end
+    
+      post "/signup" do
+        if !params[:username].empty?
+          @user = User.create(params)
+          redirect to "/tweets"
+        else
+          redirect to "/signup_error"
+        end
+      end
 
 end
